507b73be920b5e382fecc0b57837449fd4b31f30
brian <briansegs@gmail.com>
2022-09-29 14:18:52 -0400

    feat: Get and show minute data

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from kucoin.client import Market
+from kucoin.client import Market, User
 import pandas as pd
+import matplotlib.pyplot as plt
+import time
+
 from dotenv import load_dotenv
 import os
 
@@ -12,6 +15,23 @@ account_id = os.environ.get("ACCOUNT_ID")
 
 client = Market(url='https://api.kucoin.com')
 
-klines = client.get_kline('BTC-USDT','1min')
+user = User(api_key, api_secret, api_passphrase)
+# print(user.get_account_list('USDT'))
 
-print(klines)
+def getminutedata(symbol, interval, lookback):
+    currenttime = time.time()
+    starttime = currenttime - (float(lookback) * 60)
+    frame = pd.DataFrame(client.get_kline(symbol, interval, startAt=int(starttime)))
+    frame = frame.iloc[:,:6]
+    frame.columns = ['Time', 'Open', 'Close', 'High', 'Low', 'Volume']
+    frame = frame.set_index('Time')
+    frame.index = pd.to_datetime(frame.index, unit='ms')
+    frame = frame.astype(float)
+    return frame
+
+# print(getminutedata('BTC-USDT','1min', '30'))
+
+# test = getminutedata('BTC-USDT','1min', '30')
+
+# plt.plot(test.Open)
+# plt.show()
